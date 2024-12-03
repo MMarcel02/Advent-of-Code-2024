6494cd3e995a266a683698dc4d22d497fd30e314
Marcel Marciniak <marciniak.marc@gmail.com>
2024-12-03 23:11:27 +0100

    test 2

--- Day 2/script.js
+++ Day 2/script.js
@@ -1,43 +1,42 @@
 
 // Day 2 Part 1
 
-fetch('inputData.txt')
-  .then(response => response.text())
-  .then(data => {
+// fetch('inputData.txt')
+//   .then(response => response.text())
+//   .then(data => {
 
-    const reports = data.trim().split('\n').map(line => line.split(" ").map(Number));
+//     const reports = data.trim().split('\n').map(line => line.split(" ").map(Number));
   
-    let safeReports = 0; 
+//     let safeReports = 0; 
 
-    reports.forEach(report => {
+//     reports.forEach(report => {
 
-      const ascendingReport = report.slice().sort((a, b) => a - b);
-      const descendingReport = report.slice().sort((a, b) => b - a);
+//       const ascendingReport = report.slice().sort((a, b) => a - b);
+//       const descendingReport = report.slice().sort((a, b) => b - a);
 
-      const areArraysEqual = (arr1, arr2) => 
-        arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
+//       const areArraysEqual = (arr1, arr2) => 
+//         arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
       
       
-      const areLevelsWithinSafeRange = (report) => {
-        for (let i = 0; i < report.length - 1; i++) {
-          const levelDifference = Math.abs(report[i] - report[i + 1]);
-          if (levelDifference < 1 || levelDifference > 3) {
-            return false;
-          }
-        }
-        return true; 
-      };
+//       const areLevelsWithinSafeRange = (report) => {
+//         for (let i = 0; i < report.length - 1; i++) {
+//           const levelDifference = Math.abs(report[i] - report[i + 1]);
+//           if (levelDifference < 1 || levelDifference > 3) {
+//             return false;
+//           }
+//         }
+//         return true; 
+//       };
       
-      if (areArraysEqual(report, ascendingReport) || areArraysEqual(report, descendingReport)) {
-        if (areLevelsWithinSafeRange(report)) {
-          safeReports += 1
-          //lets try like this
-        }
-      } 
-    })
-    console.log(safeReports)
-  })
-  .catch(error => console.error('Error loading file:', error));
+//       if (areArraysEqual(report, ascendingReport) || areArraysEqual(report, descendingReport)) {
+//         if (areLevelsWithinSafeRange(report)) {
+//           safeReports += 1
+//         }
+//       } 
+//     })
+//     console.log(safeReports)
+//   })
+//   .catch(error => console.error('Error loading file:', error));
 
 
 // Day 2 Part 2
